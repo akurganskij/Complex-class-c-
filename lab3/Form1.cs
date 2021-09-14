4e963c109495c49ba8bb711f925d60845451893c
akurganskij <78171489+akurganskij@users.noreply.github.com>
2021-09-14 12:53:20 +0300

    Add files via upload

---
+++ lab3/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a1 = 0, a2 = 0, b1 = 0, b2 = 0;
+            bool correct = true;
+            if (textBox1.Text != "") a1 = Convert.ToDouble(textBox1.Text);
+            if (textBox2.Text != "") a2 = Convert.ToDouble(textBox2.Text);
+            if (textBox3.Text != "") b1 = Convert.ToDouble(textBox3.Text);
+            if (textBox4.Text != "") b2 = Convert.ToDouble(textBox4.Text);
+            label6.Text = "a1 = " + a1.ToString();
+            MyComplex a = new MyComplex(a1, a2);
+            MyComplex b = new MyComplex(b1, b2);
+            string oper = comboBox1.Text;
+            MyComplex c = new MyComplex();
+            if(oper == "+") c = a + b;
+            else if (oper == "-") c = a - b;
+            else if (oper == "*") c = a * b;
+            else
+            {
+                label6.Text = "Uncorret operator";
+                correct = false;
+            }
+            if(correct)label6.Text = c.print();
+
+        }
+    }
+}
---
+++ lab3/My Complex.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lab3
+{
+    internal struct Complex
+    {
+        public double Real;
+        public double Imag;
+        public Complex(double a, double b) { Real = a; Imag = b; }
+    };
+    class MyComplex
+    {
+        public Complex num;
+        public MyComplex() { num = new Complex(0.0, 0.0); }
+        protected MyComplex(Complex a) {num = a;}
+        public MyComplex(double a) { num = new Complex(a, 0.0); }
+        public MyComplex(double a, double b) { num = new Complex(a, b); }
+        public static MyComplex operator +(MyComplex a, MyComplex b)
+        {
+            Complex c = new Complex(a.num.Real + b.num.Real , a.num.Imag + b.num.Imag);                        
+            return new MyComplex(c);
+        }
+        public static MyComplex operator -(MyComplex a, MyComplex b)
+        {
+            Complex c = new Complex(a.num.Real - b.num.Real, a.num.Imag - b.num.Imag);
+            return new MyComplex(c);
+        }
+        public static MyComplex operator *(MyComplex a, MyComplex b)
+        {
+            Complex c = new Complex(a.num.Real * b.num.Real - a.num.Imag * b.num.Imag, a.num.Imag * b.num.Real + a.num.Real * b.num.Imag);
+            return new MyComplex(c);
+        }
+        public string print()
+        {
+            string s = num.Real.ToString() + " + " + num.Imag.ToString() + "i";
+            return s;
+        }
+        public double abs()
+        {
+            return Math.Sqrt(Math.Pow(num.Real, 2) + Math.Pow(num.Imag, 2));
+        }
+        public static bool operator <(MyComplex a, MyComplex b)
+        {
+            if( a.abs() < b.abs()) return true;
+            return false;
+        }
+        public static bool operator >(MyComplex a, MyComplex b)
+        {
+            if (a.abs() > b.abs()) return true;
+            return false;
+        }
+        public static bool operator ==(MyComplex a, MyComplex b)
+        {
+            if (a.abs() == b.abs()) return true;
+            return false;
+        }
+    }
+}
